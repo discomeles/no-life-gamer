d963be8bac4be1f2f37ac494c606551911a66a27
P Niemi <pauliina.niemi@kapsi.fi>
2023-09-13 17:15:08 +0300

    Added a couple of functions

--- nlg.py
+++ nlg.py
@@ -1,2 +1,25 @@
+import numpy as np
+
 # placeholder for the source code
 # add another line
+
+# Checking the status of a cell
+
+# Calculating the sum of cells around The Cell
+def calculate(A, i, j):
+    result = (A[i-1, j-1]+A[i-1, j]+A[i-1, j+1]+
+              A[i, j-1]+A[i, j+1]+
+              A[i+1, j-1]+A[i+1, j]+A[i+1, j+1])
+    return result
+
+# If cell is on the edge
+def edgy(A, i, j):
+    dimensiot = np.shape(A)
+    pass
+
+def check_submatrix(A, i: int, j: int):
+    if i == 0:
+        if j == 0:
+            pass
+    else:
+        pass
